a2d2ccfb5f2b05f1637c8ff44da45105818ea8c7
Krzysztof <krzysztof_dolanski@epam.com>
2022-09-12 14:13:39 +0200

    refactor

--- src/main/java/com/epam/library/books/functions/BookToDTOFunction.java
+++ src/main/java/com/epam/library/books/functions/BookToDTOFunction.java
@@ -2,15 +2,15 @@ package com.epam.library.books.functions;
 
 import com.epam.library.books.Book;
 import com.epam.library.books.BookDTO;
-import com.epam.library.books.functions.BookMapper;
 
 import java.util.function.Function;
 
 public class BookToDTOFunction implements Function<Book, BookDTO> {
 
+    private Function<Book, BookDTO> function = b -> new BookMapper().mapToDTO(b);
+
     @Override
     public BookDTO apply(Book book) {
-        BookMapper bookMapper = new BookMapper();
-        return bookMapper.mapToDTO(book);
+        return function.apply(book);
     }
 }
