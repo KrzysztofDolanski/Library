302e24c6c081d132c03941a139fb57cbc2fcf364
Krzysztof <krzysztof_dolanski@epam.com>
2022-09-15 15:16:31 +0200

    extended Fibonacci

---
+++ src/main/java/com/epam/tasks/tribo/Tribonacci.java
@@ -0,0 +1,32 @@
+package com.epam.tasks.tribo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Tribonacci {
+
+    public double[] tribonacci(double[] s, int n) {
+        if (s.length<1) return new double[]{};
+
+        List<Double> list = new ArrayList<>();
+
+        for(int i = 0; i<3; i++){
+            list.add(s[i]);
+        }
+
+        int i = 3;
+
+        while(i<n){
+            list.add(list.stream().sorted((x,y)->-1).limit(3).mapToDouble(x->x).reduce(Double::sum).getAsDouble());
+            i++;
+        }
+
+        double[] result = new double[list.size()];
+        for (int i1 = 0; i1 < result.length; i1++) {
+            result[i1] = list.get(i1);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/epam/tasks/tribo/TribonacciTest.java
@@ -0,0 +1,36 @@
+package com.epam.tasks.tribo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TribonacciTest {
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(new double []{1,1,1},10, new double []{1,1,1,3,5,9,17,31,57,105}),
+                Arguments.of(new double []{0,0,1},10, new double []{0,0,1,1,2,4,7,13,24,44}),
+                Arguments.of(new double []{0,1,1},10, new double []{0,1,1,2,4,7,13,24,44,81})
+        );
+    }
+
+    @ParameterizedTest(name = "{index} => arr={0}, value={1}, expected={2}")
+    @MethodSource("arguments")
+    public void shouldReturnExpectedArray(double[] arr, int value, double[] expected){
+      //given
+        Tribonacci tribonacci = new Tribonacci();
+        //when
+        double[] result = tribonacci.tribonacci(arr, value);
+        //then
+        System.out.println(Arrays.toString(expected));
+        System.out.println(Arrays.toString(result));
+        assertArrayEquals(expected, result);
+    }
+
+}
